7a471a3b6d00a132969e10ab42d4ec756b2aec57
danielw <dwatts@thoughtworks.com>
2015-11-03 23:51:52 +0000

    remove bootstrap js and move to a more react-like structured components

---
+++ components/main.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var Search = require('./search');
+
+ReactDOM.render(
+  React.createElement(Search),
+  document.getElementById('search')
+);
--- components/search.js
+++ components/search.js
@@ -1,7 +1,16 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <h1>This is a search header</h1>,
-  document.getElementById('example')
-);
+var search = React.createClass({
+	render: function () {
+		return (
+			<form method='GET' action='/api/search' className='input-group'>
+				<input className='test--search form-control' type='text' placeholder='Enter your search term' />
+				<span className='input-group-btn'>
+					<button className='btn btn-default test--search-button' type='button'>Search</button>
+				</span>
+			</form>
+			);
+	}
+});
+
+module.exports = search;
