5445e922e958d8147614a5592d4dab442aafcf0e
danielw <dwatts@thoughtworks.com>
2015-11-03 18:57:27 +0000

    rename start.js to server.js as it's more conventional and works with npm start

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.set('views', './views');
-app.set('view engine', 'jade');
+app.use('/dist', express.static('dist'));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 var server = app.listen(3000, function () {
