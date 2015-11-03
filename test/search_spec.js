e64408e4ea76f802dad64f9255a50e17869161ab
danielw <dwatts@thoughtworks.com>
2015-11-03 23:52:35 +0000

    ignore protractor angular sync and click search button

--- test/search_spec.js
+++ test/search_spec.js
@@ -1,9 +1,13 @@
 describe('Search twitter tests', function () {
 	it('should return tweets when I search for "#hudl"', function () {
+		browser.ignoreSynchronization = true;
+
 		browser.get('http://localhost:3000');
 
 		var searchTextbox = element(by.css('.test--search'));
 		searchTextbox.sendKeys('#hudl');
+
+		element(by.css('.test--search-button')).click();
 
 		var results = element.all(by.css('.test--search-results'));
 
